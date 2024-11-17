d330d71cc659441fe5755dcf20dff7c37ad2ebe4
ggomma <ggomma@gmail.com>
2024-11-17 15:28:04 +0900

    fix: include openai models env

--- src/constants/env.constants.js
+++ src/constants/env.constants.js
@@ -10,11 +10,12 @@ export const ENV_KEY = {
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   OPENAI_ORGANIZATION: process.env.OPENAI_ORGANIZATION,
   OPENAI_MODEL: process.env.OPENAI_MODEL,
+  OPENAI_MODEL_FOR_SMALL_TASK: process.env.OPENAI_MODEL_FOR_SMALL_TASK,
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
   DB_PORT: process.env.DB_PORT,
   MAIL_AUTH_USER: process.env.MAIL_AUTH_USER,
-  MAIL_AUTH_PASS: process.env.MAIL_AUTH_PASS
+  MAIL_AUTH_PASS: process.env.MAIL_AUTH_PASS,
 };
